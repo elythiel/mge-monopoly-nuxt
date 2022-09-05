235c98ffc092783ffe79cd65c463136be1d25d4b
Mickaël 'Martelle' Maillot <mickael.o.maillot@gmail.com>
2022-09-05 13:11:58 +0200

    Add meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,6 @@ import {defineNuxtConfig} from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     target: 'static',
-    head: {
-        title: 'Morgane Gervasoni - Product Owner',
-        htmlAttrs: {
-            lang: 'fr'
-        }
-    },
-
     // Global CSS: https://go.nuxtjs.dev/config-css
     css: [
         "@/assets/css/styles.css"

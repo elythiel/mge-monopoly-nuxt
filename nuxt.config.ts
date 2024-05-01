17ca256b200f144a5d4a26824bbefe2983ef6474
Mickaël 'Martelle' Maillot <mickael.o.maillot@gmail.com>
2024-05-01 10:31:06 +0200

    Add ts and eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
-export default {
+import type { NuxtConfig } from '@nuxt/types'
+
+const config: NuxtConfig = {
     target: 'static',
     css: [
-        "@/assets/css/styles.css"
+        '@/assets/css/styles.css'
     ],
-    modules: [
-        '@nuxt/content',
-        '@nuxtjs/tailwindcss'
-    ]
-};
+    buildModules: ['@nuxt/typescript-build'],
+    modules: ['@nuxt/content', '@nuxtjs/tailwindcss', '@nuxt/eslint'],
+    eslint: {
+        config: {
+            stylistic: true
+        }
+    }
+}
+
+export default config

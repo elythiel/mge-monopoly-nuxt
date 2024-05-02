6e86223755ea94f6cf45e4e86f7bd0eb86cabdb1
Mickaël 'Martelle' Maillot <mickael.o.maillot@gmail.com>
2024-05-02 11:44:53 +0200

    [content] Update footer copyright

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -1,6 +1,10 @@
+<script lang="ts" setup>
+const currentYear = (new Date()).getFullYear();
+</script>
+
 <template>
   <div class="text-gray-400 mx-8 lg:mx-16 mb-4 text-base lg:text-lg 2xl:text-xl 2xl:ml-24 2xl:mb-6">
-    © 2022 - Dashdone
+    © {{ currentYear }} - Dashdone
     <span class="mx-2">
       |
     </span>
